2360e6e893c5275c8b44c8c3c16497d08fc61fe7
Ruthvik Kamarasu <kamarasuruthvik@gmail.com>
2024-09-04 13:05:28 -0700

    Quest 2: finished data types and sum!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,14 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+        int number1 = 2;
+        int number2 = 4;
+        char letter = 'c';
+        String word = "Hello";
+        double decimalNumber = 45.8;
+
+        int z = number1 + number2;
+        System.out.print(z);
     }
 }

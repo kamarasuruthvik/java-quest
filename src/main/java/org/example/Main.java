09c2a8a7569fff3a99667f491b7161864b5e2d81
Ruthvik Kamarasu <kamarasuruthvik@gmail.com>
2024-09-11 13:13:22 -0700

    quest 5: task 1 completed creating a perosn class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,14 +6,7 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {10, 11, 1, 2, 3};
-        int high = -1000, low = 1000;
-        for(int i: numbers){
-            high = Math.max(high, i);
-            low = Math.min(low, i);
-        }
-
-        System.out.println("The highest number is: "+ high);
-        System.out.println("The lowest number is: "+ low);
+        Person ruthvik = new Person("Ruthvik", 24, "Student");
+        ruthvik.displayDetails();
     }
 }
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Person {
+    public String name;
+    public int age;
+    public String occupation;
+
+    public Person(String name, int age, String occupation){
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+    }
+
+    public void displayDetails(){
+        System.out.println(name+ " " + age + " " + occupation);
+    }
+}

9ac0d1488373467486af73c6a4ec47afb70e8469
Ruthvik Kamarasu <kamarasuruthvik@gmail.com>
2024-09-24 16:10:38 -0700

    feat: quest 11: task 3 working with in built java functional interfaces complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,11 @@
 package org.example;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.Consumer;
+import java.util.function.BiPredicate;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -12,12 +15,33 @@ interface MathOperation{
 }
 public class Main {
     public static void main(String[] args) {
-        MathOperation add = (a, b) -> a+b ;
-        MathOperation subtract = (a, b) -> a-b;
-        MathOperation multiply = (a, b) -> a*b;
+        // four different types of built-in interfaces
+        //1 : Supplier. This interface ALWAYS returns something and DOESN'T accept ANYTHING
+        //The built-in method is get
+        Supplier<Integer> supplier = () -> 100;
+        System.out.println(supplier.get());
 
-        System.out.println("add: " + add.operate(3, 4));
-        System.out.println("subtract: " + subtract.operate(3, 4));
-        System.out.println("multiply: " + multiply.operate(3, 4));
+        //2: Consumer. This interface ALWAYS accepts something and returns nothing
+        //method is accept
+        // there is also Bi-Consumer
+        Consumer<Integer>  consumer = (a) -> System.out.print(a + " ");
+        consumer.accept(200);
+
+        // example of using consumer with for each
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        arr.forEach(consumer);
+        System.out.println();
+        //3: Predicate: This interface ALWAYS takes a value does a logical test and returns a boolean
+        //method is test
+        BiPredicate<Integer, Integer> biPredicate = (a, b) -> a> b;
+        System.out.println(biPredicate.test(3, 4));
+
+        //4: Function. Takes AN ARGUMENT and RETURNS a VALUE
+        // method is apply
+        Function<Integer, Integer> square = n -> n*n;
+        System.out.println(square.apply(2));
+
+
+
     }
 };

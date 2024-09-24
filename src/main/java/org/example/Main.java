1fdebdb4e67f4e40723659b81421fa68ca945954
Ruthvik Kamarasu <kamarasuruthvik@gmail.com>
2024-09-24 14:17:25 -0700

    feat: quest 11: task 1 without lambda expression but with functional interface

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,47 +2,21 @@ package org.example;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-class Counter
-{
-    int count;
-
-    public synchronized void increment(){
-        count++;
-    }
-
+@FunctionalInterface
+interface A{
+    void printSomething();
 }
+
+
 public class Main {
     public static void main(String[] args) {
-        Counter c = new Counter();
+        A obj = new A(){
+            @Override
+            public void printSomething() {
+                System.out.println("in Show!");
+            }
+        };
 
-        try{
-            Thread t1= new Thread(new Runnable(){
-            public void run()
-                    {
-                        for(int i=1; i<=1000; i++){
-                            c.increment();
-                        }
-                    }
-            });
-
-            Thread t2= new Thread(new Runnable(){
-                public void run()
-                {
-                    for(int i=1; i<=1000; i++){
-                        c.increment();
-                    }
-                }
-            });
-
-            t1.start();
-            t2.start();
-
-            t1.join();
-            t2.join();
-
-        } catch (Exception e){
-            System.out.println("Throws error: "+ e.getMessage());
-        }
-        System.out.println("Count: "+ c.count);
+        obj.printSomething();
     }
 }

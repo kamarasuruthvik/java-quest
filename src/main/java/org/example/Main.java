6775ca4347865df126457e43f57f3ab6c17d0f08
Ruthvik Kamarasu <kamarasuruthvik@gmail.com>
2024-09-24 15:07:11 -0700

    feat: quest 11: task 1 using lambda expression

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,21 @@
 package org.example;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-@FunctionalInterface
-interface A{
-    void printSomething();
-}
-
-
 public class Main {
     public static void main(String[] args) {
-        A obj = new A(){
-            @Override
-            public void printSomething() {
-                System.out.println("in Show!");
-            }
-        };
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1,2,3,4));
+        arr.forEach((a)-> System.out.print(a+" "));
 
-        obj.printSomething();
+        //after sorting
+        arr.sort((a,b)-> b-a);
+        System.out.println();
+        System.out.println(arr);
+
+
     }
-}
+};

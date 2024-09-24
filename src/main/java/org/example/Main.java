c6e4bb467e62b0db83b0c94529fa10a1eb9b103b
Ruthvik Kamarasu <kamarasuruthvik@gmail.com>
2024-09-23 22:49:56 -0700

    feat: quest 10: task 1 complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,52 +2,33 @@ package org.example;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+class Example extends Thread{
+    public int threadNum = 1;
 
-class Hi implements Runnable{
-    public void run(){
-        for(int i=0; i<5; i++){
-            System.out.println("Hi");
-            try {
-                Thread.sleep(500);
-            } catch(Exception e){
-                System.out.println(e.getMessage());
-            }
-        }
+     Example(int threadNum){
+        this.threadNum = threadNum;
     }
-}
 
-class Hello implements Runnable{
     public void run(){
+        System.out.println("Thread" + threadNum + "is running");
         for(int i=0; i<5; i++){
-            System.out.println("Hello");
-            try {
+            try{
+                System.out.println(threadNum+ ": " + i);
                 Thread.sleep(500);
-            } catch(Exception e){
-                System.out.println(e.getMessage());
+            } catch (Exception e){
+                System.out.println(threadNum + "raised exception "+ e.getMessage());
             }
         }
     }
 }
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Hi obj1 = new Hi();
-        Hello obj2 = new Hello();
+    public static void main(String[] args) {
+        Example c1 = new Example(1), c2 = new Example(2);
+        c1.start();
+        c2.start();
 
-        Thread t1 = new Thread(obj1, "Hi Thread");
-        Thread t2 = new Thread(obj2, "Hello Thread");
+        System.out.println("Threads finished execution");
 
-        t1.start();
-        t2.start();
-        try{
-            Thread.sleep(10);
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-        System.out.println(t1.isAlive());
-        t1.join();
-        t2.join();
-
-        System.out.println("Bye!");
     }
 }

8d1e99b9ca8cc38d5cf8514b121673c33127487d
Ruthvik Kamarasu <kamarasuruthvik@gmail.com>
2024-09-25 16:58:19 -0700

    Quest 11: Task 1, Task 2, Task 3 complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,55 @@
 package org.example;
-
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.function.Consumer;
-import java.util.function.BiPredicate;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-@FunctionalInterface
-interface MathOperation{
-    public int operate(int a, int b);
-}
 public class Main {
     public static void main(String[] args) {
-        // four different types of built-in interfaces
-        //1 : Supplier. This interface ALWAYS returns something and DOESN'T accept ANYTHING
-        //The built-in method is get
-        Supplier<Integer> supplier = () -> 100;
-        System.out.println(supplier.get());
+        //TASK 1:
+        //Create a list of integers and use Streams to filter out even numbers.
+        //Map each number to its square and collect the results into a new list.
+        //Print the resulting list using a stream.
+        System.out.println("TASK 1:");
+        List<Integer> nums = Arrays.asList(4,6,2, 1, 9);
+        List<Integer> data = nums.stream().filter((n)-> n%2==0).map((n)->n*n).collect(Collectors.toList());
+        data.forEach((n)-> System.out.println(n));
+        System.out.println("\n");
 
-        //2: Consumer. This interface ALWAYS accepts something and returns nothing
-        //method is accept
-        // there is also Bi-Consumer
-        Consumer<Integer>  consumer = (a) -> System.out.print(a + " ");
-        consumer.accept(200);
-
-        // example of using consumer with for each
-        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        arr.forEach(consumer);
-        System.out.println();
-        //3: Predicate: This interface ALWAYS takes a value does a logical test and returns a boolean
-        //method is test
-        BiPredicate<Integer, Integer> biPredicate = (a, b) -> a> b;
-        System.out.println(biPredicate.test(3, 4));
-
-        //4: Function. Takes AN ARGUMENT and RETURNS a VALUE
-        // method is apply
-        Function<Integer, Integer> square = n -> n*n;
-        System.out.println(square.apply(2));
+        //TASK 2:
+        //Filter out strings that are empty or null.
+        //Sort the strings in alphabetical order.
+        //Convert each string to uppercase and collect the results into a new list.
+        System.out.println("TASK 2:");
+        List<String> names = Arrays.asList("Ruthvik", "Arthi", "Varshith", "Baals", "");
+        List<String> finalNames = names.stream()
+                .filter((n) -> n!="")
+                .sorted()
+                .map((n) -> n.toUpperCase())
+                .collect(Collectors.toList());
 
 
+        finalNames.forEach((n)-> System.out.println(n));
+
+        System.out.println("\n");
+        //TASK 3:
+        //Use the reduce() method to sum up all the numbers in a list.
+        //Use the collect() method to gather stream results into a specific type of collection, like a Set.
+        //Use findFirst() and anyMatch() to demonstrate how to retrieve specific elements from the stream.
+        System.out.println("TASK 3:");
+        List<Integer> newNums = Arrays.asList(1, 4, 5, 7, 10, 6);
+        int sum = newNums.
+                stream().
+                reduce(0, (a, b)-> a+b);
+
+        Set<Integer> uniques = newNums.stream().collect(Collectors.toSet());
+
+        //using anyMatch to check if any number is even
+        boolean hasEven = newNums.stream().anyMatch(n -> n%2 == 0);
+        System.out.println(sum+ "\n" + uniques +"\n" + hasEven);
 
     }
 };

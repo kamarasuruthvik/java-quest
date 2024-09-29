276fb72116f3b3ae532070322e9138becb9575f5
Ruthvik Kamarasu <kamarasuruthvik@gmail.com>
2024-09-29 16:45:07 -0700

    quest 13: refined method for removing books and listing all books

--- src/main/java/org/example/Library/Library.java
+++ src/main/java/org/example/Library/Library.java
@@ -8,32 +8,35 @@ public class Library {
     ArrayList<Book> books = new ArrayList<>();
 
 
-    void addBook(Book book){
+    public void addBook(Book book){
         books.add(book);
         System.out.println("Book is added: "+ book);
     }
 
-    void removeBook(String isbn){
+    public void removeBook(String isbn){
+        System.out.println("Removing books...");
         Optional<Book> bookToRemove = books
                 .stream()
                 .filter(i -> i.getISBN().equals(isbn)).findFirst();
-
-        if(bookToRemove.isEmpty()){
-            books.remove(bookToRemove);
+        if(!bookToRemove.isEmpty()){
+            books.remove(bookToRemove.get());
             System.out.println("The book " +bookToRemove +" is Removed!");
         } else {
             System.out.println("The book with ISBN:" + isbn + " is not found.");
         }
     }
 
-    List<Book> searchBooks(String keyword) {
+    public List<Book> searchBooks(String keyword) {
         return books.stream()
                 .filter(book -> book.getTitle().toLowerCase().contains(keyword) ||
                         book.getAuthor().toLowerCase().contains(keyword))
                 .toList();
     }
 
-    void listAllBooks(){
-        books.stream().forEach(book -> System.out.println(book));
+    public void listAllBooks(){
+        books.stream().forEach(book -> {
+            System.out.print("Title: "+ book.getTitle() +" @ " +" ISBN: " +book.getISBN());
+        });
+        System.out.println();
     }
 }
